18da418aa410881cbe0ee3e2360d33dd02edb8d3
miguelsantoss01 <miguel.011zn@gmail.com>
2024-10-10 11:49:21 -0300

    criando método post

--- mundodofunk/funks/models.py
+++ mundodofunk/funks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Funk(models.Model):
+    nome = models.CharField(max_length=255)
+    artista = models.CharField(max_length=255)
+    duracao = models.TimeField()
+    descricao = models.TextField()
+    data = models.DateField()
+    logo = models.URLField()
+
+    def __str__(self):
+            return self.title
--- mundodofunk/funks/views.py
+++ mundodofunk/funks/views.py
@@ -1,16 +1,29 @@
+import json
 from django.http import JsonResponse, HttpResponseBadRequest
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
+
+from funks.models import Funk
 
 @require_http_methods(["GET", "POST", "PUT", "DELETE"])
 @csrf_exempt
 def FunkView(request):
     if request.method == 'GET':
-        data = {"message": "GET request recebida meu camarada"}
+        data = {"message": "GET request recebida meu camaradaaaaa"}
         return JsonResponse(data)
     
     elif request.method == 'POST':
-        data = {"message": "POST request Davi"}
+        data = json.loads(request.body)
+        funk = Funk(
+            nome = data.get("nome"),
+            artista = data.get("artista"),
+            duracao = data.get("duracao"),
+            descricao = data.get("descricao"),
+            data = data.get("data"),
+            logo = data.get("logo"),
+        )
+        funk.save()
+        
         return JsonResponse(data)
     
     elif request.method == 'PUT':
